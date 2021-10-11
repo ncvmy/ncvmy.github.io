50942ac6a14997c9905dba61b8bd3d1d63edb602
ncvmy <darkgladit2015@gmail.com>
2021-10-11 21:43:57 +0700

    Điều Khiển Ứng Dụng Project
    Điều Khiển Ứng Dụng Project

---
+++ DieuKhienUngDung/DieuKhienUngDung/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace DieuKhienUngDung
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button6 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mở notepad";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(130, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Mở ứng dụng bất kì";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Mở CMD";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(130, 41);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Mở CMD ẩn";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(248, 41);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(190, 23);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Mở CMD trả về kết quả";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 70);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(112, 23);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(130, 70);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(112, 23);
+            this.numericUpDown2.TabIndex = 1;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(462, 68);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(148, 23);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Click tọa độ màn hình";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(248, 70);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(109, 19);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Kích chuột phải";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(363, 70);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(93, 19);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Double Click";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(130, 99);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(148, 23);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "Click tọa độ ứng dụng";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(130, 128);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(308, 23);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "Click tọa độ ứng dụng không cần tọa độ";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 129);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 381);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ DieuKhienUngDung/DieuKhienUngDung/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms;
+using KAutoHelper;
+namespace DieuKhienUngDung
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Process start notepad
+            Process.Start("notepad");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Process start notepad
+            Process process = new Process();
+            process.StartInfo.UseShellExecute = true;
+            //Process file name chúa url 
+            process.StartInfo.FileName= "D:\\ảnh\\105APPLE\\IMG_5001.jpg";
+            process.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string content = "/C ping -t howkteam.com";
+            //Process mở cmd và attribute đầu vào là nội dung để run CMD
+            Process.Start("CMD.exe", content);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string strCmdText;
+            strCmdText = @"/C ""D:\\ảnh\\105APPLE\\IMG_5001.jpg""";
+
+            Process p = new Process();
+            //Process mở cmd và attribute đầu vào là nội dung để run CMD
+            p.StartInfo.FileName = "CMD.exe";
+            p.StartInfo.Arguments = strCmdText;
+
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            p.Start();
+
+            //p.Kill();
+
+            /*
+             
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = @"/C ""How Kteam - Free Education.html""";
+            process.StartInfo = startInfo;
+            process.Start();
+             
+             */
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string strCmdText;
+            strCmdText = "ping howkteam.com";
+
+            Process p = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            //Process mở cmd và attribute đầu vào là nội dung để run CMD
+            startInfo.FileName = "CMD.exe";
+            //Không xuất hiện window cmd
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            //Xét cờ input
+            startInfo.RedirectStandardInput = true;
+            //Xét cờ output
+            startInfo.RedirectStandardOutput = true;
+            p.StartInfo = startInfo;
+            //start cmd
+            p.Start();
+            //Viết ra cmd nội dung muốn thực hiện
+            p.StandardInput.WriteLine(strCmdText);
+            //Làm sạch data
+            p.StandardInput.Flush();
+            //Đóng
+            p.StandardInput.Close();
+            //Chờ xử lý thực hiện xong
+            p.WaitForExit();
+            //Get toàn bộ ouput trả về
+            string result = p.StandardOutput.ReadToEnd();
+
+            MessageBox.Show(result);
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int x = (int)numericUpDown1.Value;
+            int y = (int)numericUpDown2.Value;
+
+            EMouseKey mouseKey = EMouseKey.LEFT;
+
+            if (checkBox1.Checked)
+            {
+                if (checkBox2.Checked)
+                {
+                    mouseKey = EMouseKey.DOUBLE_RIGHT;
+                }
+                else
+                {
+                    mouseKey = EMouseKey.RIGHT;
+                }
+            }
+            else
+            {
+                if (checkBox2.Checked)
+                {
+                    mouseKey = EMouseKey.DOUBLE_LEFT;
+                }
+            }
+
+            //Cursor.Position = new Point(x,y);
+            AutoControl.MouseClick(x, y, mouseKey);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int x = (int)numericUpDown1.Value;
+            int y = (int)numericUpDown2.Value;
+            //Lấy name mainwindow
+            // var name = Process.GetProcessById(21964);
+            // MessageBox.Show(name.ProcessName);
+            //Set mainWindowHandle
+            //var hWnd = Process.GetProcessById(12012).MainWindowHandle;
+            //var hWnd = Process.GetProcessesByName("Remote Desktop Connection")[0].MainWindowHandle;
+            IntPtr hWnd = IntPtr.Zero;
+            //Lấy name ứng dụng muốn lấy tọa độ
+            hWnd = AutoControl.FindWindowHandle(null, textBox1.Text);
+
+            // lấy ra tọa độ trên màn hình của tọa độ bên trong cửa sổ
+            var pointToClick = AutoControl.GetGlobalPoint(hWnd, x, y);
+
+            EMouseKey mouseKey = EMouseKey.LEFT;
+
+            if (checkBox1.Checked)
+            {
+                if (checkBox2.Checked)
+                {
+                    mouseKey = EMouseKey.DOUBLE_RIGHT;
+                }
+                else
+                {
+                    mouseKey = EMouseKey.RIGHT;
+                }
+            }
+            else
+            {
+                if (checkBox2.Checked)
+                {
+                    mouseKey = EMouseKey.DOUBLE_LEFT;
+                }
+            }
+
+            AutoControl.BringToFront(hWnd);
+
+            AutoControl.MouseClick(pointToClick, mouseKey);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int x = (int)numericUpDown1.Value;
+            int y = (int)numericUpDown2.Value;
+            IntPtr hWnd = IntPtr.Zero;
+            //Lấy name ứng dụng muốn lấy tọa độ
+            hWnd = AutoControl.FindWindowHandle(null, textBox1.Text);
+
+            // lấy ra tọa độ trên màn hình của tọa độ bên trong cửa sổ
+            IntPtr hWndChil = IntPtr.Zero;
+            hWndChil = AutoControl.FindHandle(hWnd, textBox2.Text, textBox2.Text);
+            var pointToClick = AutoControl.GetGlobalPoint(hWndChil, x, y);
+
+            EMouseKey mouseKey = EMouseKey.LEFT;
+
+            if (checkBox1.Checked)
+            {
+                if (checkBox2.Checked)
+                {
+                    mouseKey = EMouseKey.DOUBLE_RIGHT;
+                }
+                else
+                {
+                    mouseKey = EMouseKey.RIGHT;
+                }
+            }
+            else
+            {
+                if (checkBox2.Checked)
+                {
+                    mouseKey = EMouseKey.DOUBLE_LEFT;
+                }
+            }
+
+            AutoControl.BringToFront(hWnd);
+
+            AutoControl.MouseClick(pointToClick, mouseKey);
+        }
+    }
+}
